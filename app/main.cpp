13aab7516588ed2218cc1be353d087e793752f77
gokalpcevik <cevikgokalp8@gmail.com>
2023-03-20 20:48:12 +0300

    remove old files

--- app/main.cpp
+++ app/main.cpp
@@ -9,7 +9,7 @@
 #define SDL_MAIN_HANDLED
 
 #include <iostream>
-#include "realsim/core/Application.h"
+#include "realsim/core/App.h"
 #include "realsim/core/Window.h"
 
 #include "assetlib/AssetLoader.h"
@@ -17,43 +17,12 @@
 
 #include <thread>
 
-namespace Core = ::RSim::Core;
-namespace AssetLib = ::RSim::AssetLib;
-
-class RealSimInteractive : public Core::Application
-{
-public:
-	RealSimInteractive(CommandLineArgs args);
-
-	RSim::ReturnCode OnInit() override;
-	void OnUpdate() override;
-
-private:
-	AssetLib::Asset MeshAsset;
-	AssetLib::MeshInfo MeshInfo;
-};
-
-RealSimInteractive::RealSimInteractive(CommandLineArgs args) : Application(args)
-{
-}
-
-RSim::ReturnCode RealSimInteractive::OnInit()
-{
-	return Application::OnInit();
-}
-
-void RealSimInteractive::OnUpdate()
-{
-	Application::OnUpdate();
-}
-
+using namespace RSim;
 
 int main(int argc, char** argv)
 {
-	auto* application = new RealSimInteractive({argc, argv});
-
-	Core::WindowDescription desc{ "RealSim", 800, 600, SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,SDL_WINDOW_RESIZABLE};
-	auto window = std::make_unique<RSim::Core::Window>(desc);
-	application->SetMainWindow(std::move(window));
-	return application->Run();
+	Core::WindowDescription desc{ "RealSim", 800, 600, SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,SDL_WINDOW_RESIZABLE|SDL_WINDOW_MAXIMIZED};
+	auto window = std::make_unique<Core::Window>(desc);
+	Core::App::Get().SetMainWindow(std::move(window));
+	return Core::App::Get().Run();
 }
